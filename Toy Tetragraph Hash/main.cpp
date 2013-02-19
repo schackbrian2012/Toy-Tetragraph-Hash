ac7783b27ab53924da281296b7e003e21e8f1601
Brian Schack <brianschack@macbookpro-a32f.morehead-st.edu>
2013-02-19 12:57:42 -0500

    Initial Commit

---
+++ Toy Tetragraph Hash/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Toy Tetragraph Hash
+//
+//  Created by Brian Schack on 2/19/13.
+//  Copyright (c) 2013 Brian Schack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
